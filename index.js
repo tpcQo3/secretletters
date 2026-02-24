9924161f8f7ac600cd1940ac3e7329ffc86d4828
An Binh <lunarclientqt3333@gmail.com>
2026-02-24 21:44:39 +0700

    add first letter

--- index.js
+++ index.js
@@ -1,5 +1,33 @@
-const envelope = document.getElementById("envelope");
+function createLink(){
+    let name = document.getElementById("name").value;
+    let msg = document.getElementById("msg").value;
 
-envelope.addEventListener("click", () => {
-    envelope.classList.toggle("open");
-});
+    if(name === "" || msg === ""){
+        alert("Please fill all fields");
+        return;
+    }
+
+    let data = btoa(name + "||" + msg);
+    let link = window.location.origin + "?data=" + data;
+
+    document.getElementById("result").innerHTML =
+        "Share this link:<br><a href='"+link+"'>"+link+"</a>";
+}
+
+function readSecret(){
+    let params = new URLSearchParams(window.location.search);
+    let data = params.get("data");
+
+    if(data){
+        let decoded = atob(data);
+        let parts = decoded.split("||");
+
+        document.getElementById("mainBox").innerHTML = `
+            <h2>Secret Letter</h2>
+            <p><b>From:</b> ${parts[0]}</p>
+            <p>${parts[1]}</p>
+        `;
+    }
+}
+
+readSecret();
