4957cfb077799a8e72d796b6614f04251e412251
An Binh <lunarclientqt3333@gmail.com>
2026-02-24 21:56:28 +0700

    letter

--- script.js
+++ script.js
@@ -1,25 +1,9 @@
-function openLetter(){
-    document.getElementById("letterContainer").classList.add("hidden");
-    document.getElementById("letterContent").classList.remove("hidden");
+function openLetter() {
+    const envelope = document.querySelector(".envelope");
+    envelope.classList.toggle("open");
 }
 
-function showForm(){
-    document.getElementById("letterContent").classList.add("hidden");
-    document.getElementById("mainBox").classList.remove("hidden");
-}
-
-function createLink(){
-    let name = document.getElementById("name").value;
-    let msg = document.getElementById("msg").value;
-
-    if(name === "" || msg === ""){
-        alert("Please fill all fields");
-        return;
-    }
-
-    let data = btoa(name + "||" + msg);
-    let link = window.location.origin + "?data=" + data;
-
-    document.getElementById("result").innerHTML =
-        "Share this link:<br><a href='"+link+"'>"+link+"</a>";
+function goNext(event) {
+    event.stopPropagation();
+    window.location.href = "write.html";
 }
