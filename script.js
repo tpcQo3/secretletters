a18b75383cc244751f48250a46a9d53ae2b42c6a
An Binh <lunarclientqt3333@gmail.com>
2026-02-25 17:42:00 +0700

    abcd

--- script.js
+++
@@ -1,6 +0,0 @@
-const envelope = document.querySelector(".envelope");
-const scene = document.querySelector(".scene");
-
-envelope.addEventListener("click", () => {
-  scene.classList.add("open");
-});
