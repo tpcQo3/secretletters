090f8f23948d3bd1abeead8e107cab704cae70d4
An Binh <lunarclientqt3333@gmail.com>
2026-02-25 13:31:10 +0700

    abc

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
 const envelope = document.querySelector(".envelope");
-const letter = document.querySelector(".letter");
 
 envelope.addEventListener("click", () => {
   envelope.classList.add("open");
-
-  setTimeout(() => {
-    letter.style.display = "block";
-  }, 400);
 });
